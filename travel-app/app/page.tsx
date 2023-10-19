ac69c6446a4d5ba0bb30efacf523bd45e3f20516
David <davidnezri1990@gmail.com>
2023-10-19 17:37:18 +0300

    update page component

--- travel-app/app/page.tsx
+++ travel-app/app/page.tsx
@@ -1,9 +1,18 @@
+import Camp from "@/Components/Camp"
+import Features from "@/Components/Features"
+import GetApp from "@/Components/GetApp"
+import Guide from "@/Components/Guide"
+import Hero from "@/Components/Hero"
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500 text-2xl">
-        hello world
-    </h1>
+    <>
+       <Hero/> 
+       <Camp/> 
+       <Guide/> 
+       <Features/> 
+       <GetApp/> 
+    </>
 
   )
 }
