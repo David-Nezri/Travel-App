a2b7fc6855470fdd7b52cc21825cea4f1e108efd
David <davidnezri1990@gmail.com>
2023-10-19 13:55:46 +0300

    define layout of the app

--- travel-app/app/page.tsx
+++ travel-app/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">
+    <h1 className="text-bold text-teal-500 text-2xl">
         hello world
     </h1>
 
